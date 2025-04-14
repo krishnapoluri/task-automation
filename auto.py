c1f1dacfb95100907f5e076438af5bbf826e0081
Adhwaith Krishna <krishna86396@gmail.com>
2025-04-15 00:09:44 +0530

    taskautomation.py

--- auto.py
+++ auto.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # Define the directory to organize
-source_directory = '/Users/APPLE/Desktop/Ak'  # Change this to your directory
+source_directory = '/Users/APPLE/Desktop/Ak'  # Change this according to your directory
 organized_directory = os.path.join(source_directory, 'Organized')
 
 # Create folders for organizing files
